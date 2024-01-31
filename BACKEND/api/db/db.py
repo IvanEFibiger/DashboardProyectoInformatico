26982858d0eb6af8b5c8e6dfb588d4bbecc7fc1a
Ivan Ever Fibiger <115630241+IvanEFibiger@users.noreply.github.com>
2024-01-31 18:32:41 -0300

    Update db.py

--- BACKEND/api/db/db.py
+++ BACKEND/api/db/db.py
@@ -3,7 +3,7 @@ from api import app
 # Configuración de la base de datos y la clave secreta
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'user_api_flask'
-app.config['MYSQL_PASSWORD'] ='19Ivan88'
+app.config['MYSQL_PASSWORD'] ='12324'
 app.config['MYSQL_DB'] = 'db_proyecto_informatico'
 app.config['SECRET_KEY'] = 'app_123'
 
